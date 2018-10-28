2e3aa65aa2137cab734d0bf83f0d80bc25cb0e46
BBuchholz <brentbuchholz@gmail.com>
2018-10-28 09:07:48 -0500

    add working drawer to synergy

--- app/src/main/java/com/nineworldsdeep/synergy/SynergyActivity.kt
+++ app/src/main/java/com/nineworldsdeep/synergy/SynergyActivity.kt
@@ -3,7 +3,11 @@ package com.nineworldsdeep.synergy
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.design.widget.NavigationView
+import android.support.v4.view.GravityCompat
 import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.ActionBar
+import android.support.v7.widget.Toolbar
+import android.view.MenuItem
 import com.nineworldsdeep.synergy.ui.synergy.SynergyFragment
 
 class SynergyActivity : AppCompatActivity() {
@@ -13,6 +17,15 @@ class SynergyActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.synergy_activity)
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val actionBar: ActionBar? = supportActionBar
+        actionBar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+            setHomeAsUpIndicator(R.drawable.ic_menu)
+        }
 
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
@@ -39,4 +52,15 @@ class SynergyActivity : AppCompatActivity() {
         }
     }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        return when (item.itemId) {
+
+            android.R.id.home -> {
+                mDrawerLayout.openDrawer(GravityCompat.START)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
 }
